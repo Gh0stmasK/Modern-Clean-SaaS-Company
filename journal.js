1470e1e4c1e372b6142ba06e03444535fab9dd68
Gh0stmasK <soura20v25@gmail.com>
2025-12-06 22:42:47 +0530

    new file:   article/Carbon-Accounting-Myths-Models-and-Must-Haves-Assets/Image-1.svg 	new file:   article/Carbon-Accounting-Myths-Models-and-Must-Haves-Assets/Image-2.svg 	new file:   article/Carbon-Accounting-Myths-Models-and-Must-Haves-Assets/Image.svg 	new file:   article/How-to-Build-a-Climate-Ready-Data-Stack-Assets/Hero-Image-1.svg 	new file:   article/How-to-Build-a-Climate-Ready-Data-Stack-Assets/Hero-Image-2.svg 	new file:   article/How-to-Build-a-Climate-Ready-Data-Stack-Assets/Hero-Image.svg 	new file:   article/How-to-Build-a-Climate-Ready-Data-Stack.css 	new file:   article/How-to-Build-a-Climate-Ready-Data-Stack.html 	modified:   journal.html 	modified:   journal.js

--- journal.js
+++ journal.js
@@ -1,3 +1,7 @@
-document.getElementById('article-1').onclick = function() {
+document.getElementById('article-1').onclick = function () {
+  window.location.href = 'article/How-to-Build-a-Climate-Ready-Data-Stack.html';
+};
+
+document.getElementById('article-5').onclick = function () {
   window.location.href = 'article/Carbon-Accounting-Myths-Models-and-Must-Haves.html';
 };
