3a79fc5e480797e7ab17d531f0322ba936f4cca8
Gh0stmasK <soura20v25@gmail.com>
2025-12-07 23:38:25 +0530

    modified:   journal.js

--- journal.js
+++ journal.js
@@ -2,6 +2,10 @@ document.getElementById('article-1').onclick = function () {
   window.location.href = 'article/How-to-Build-a-Climate-Ready-Data-Stack.html';
 };
 
+document.getElementById('article-2').onclick = function () {
+  window.location.href = 'article/Sustainability-Isn’t-a-Side-Project-Making-Impact-Operational.html';
+};
+
 document.getElementById('article-5').onclick = function () {
   window.location.href = 'article/Carbon-Accounting-Myths-Models-and-Must-Haves.html';
 };
