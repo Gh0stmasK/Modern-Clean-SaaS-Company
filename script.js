26673697c04aa0aa2bab84d5973ded80f0a778f0
Gh0stmasK <soura20v25@gmail.com>
2025-12-10 14:16:05 +0530

    modified:   index.html 	new file:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-icon').onclick = function () {
+    document.querySelector('header').style.backgroundColor = '#ffffff';
+    document.querySelector('.navmenu-items').style.display = 'flex';
+}
