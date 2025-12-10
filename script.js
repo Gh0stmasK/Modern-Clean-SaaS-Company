0d51896e720cfc72a1e13fb245ac5eff70d257f3
Gh0stmasK <soura20v25@gmail.com>
2025-12-10 17:16:53 +0530

    modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.getElementById('mobile-menu-icon').onclick = function () {
-    // document.querySelector('header').style.backgroundColor = '#ffffff';
-    // document.querySelector('.navmenu-items').style.display = 'flex';
-
-    var x = document.getElementById('navmitems');
-    if (x.style.display === 'none') {
-        x.style.display = 'flex';
-        document.querySelector('header').style.backgroundColor = '#ffffff';
+    const menu = document.getElementById('navmitems');
+    const header = document.querySelector('header');
+    
+    menu.classList.toggle('open');
+    
+    if (menu.classList.contains('open')) {
+        header.style.backgroundColor = '#ffffff';
     } else {
-        x.style.display = 'none';
-        document.querySelector('header').style.backgroundColor = 'transparent';
+        header.style.backgroundColor = 'transparent';
     }
 }
 
