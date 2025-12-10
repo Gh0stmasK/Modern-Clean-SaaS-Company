3f23eb89ee141d078b5b339841c6e468405a903f
Gh0stmasK <soura20v25@gmail.com>
2025-12-10 12:27:06 +0530

    modified:   about.css 	modified:   article/Carbon-Accounting-Myths-Models-and-Must-Haves.css 	modified:   article/How-to-Build-a-Climate-Ready-Data-Stack.css 	modified:   article/Inside-the-Aetherfield-Model-How-We-Turn-Data-Into-Action.css 	modified:   "article/Sustainability-Isn\342\200\231t-a-Side-Project-Making-Impact-Operational.css" 	modified:   careers.css 	modified:   careers.js 	new file:   job-listing/Data-Scientist.css 	new file:   job-listing/Data-Scientist.html 	new file:   job-listing/Product-Manager.css 	new file:   job-listing/Product-Manager.html 	modified:   job-listing/UX-Designer.css 	modified:   journal.css 	modified:   style.css

--- careers.js
+++ careers.js
@@ -1,3 +1,11 @@
 document.getElementById('view-ux-designer').onclick = function () {
   window.location.href = 'job-listing/UX-Designer.html';
 };
+
+document.getElementById('view-data-scientist').onclick = function () {
+  window.location.href = 'job-listing/Data-Scientist.html';
+};
+
+document.getElementById('view-product-manager').onclick = function(){
+  window.location.href = 'job-listing/Product-Manager.html';
+}
